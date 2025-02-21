8bc0ddc563a0c909a1c5a1a078f175aeeeefcdc7
Baizx98 <baizx98@gmail.com>
2025-02-21 17:20:28 +0800

    [Misc] Add offline inference example with layer-wise block manager.
    Signed-off-by: Baizx98 <baizx98@gmail.com>

---
+++ examples/offline_inference_layer_block_manager.py
@@ -0,0 +1,33 @@
+import os
+
+from vllm import LLM, SamplingParams
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# Sample prompts.
+prompts = [
+    "Hello, my name is",
+    "The president of the United States is",
+    "The capital of France is",
+    "The future of AI is",
+]
+# Create a sampling params object.
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+
+# Create an LLM.
+llm = LLM(
+    model="/Tan/model/Llama-3.2-1B-Instruct",
+    gpu_memory_utilization=0.9,
+    max_model_len=2048,
+    enable_chunked_prefill=False,
+    enforce_eager=True,
+    enable_layer_wise_block=True,
+)
+# Generate texts from the prompts. The output is a list of RequestOutput objects
+# that contain the prompt, generated text, and other information.
+outputs = llm.generate(prompts, sampling_params)
+# Print the outputs.
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
