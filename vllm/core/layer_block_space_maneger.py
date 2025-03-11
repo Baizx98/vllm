3ccd6536cff83fa349e4b77d7d89b8850d1b9e3d
Baizx98 <baizx98@gmail.com>
2025-03-11 20:00:11 +0800

    [Core] add interface to get layer_block_table
    Signed-off-by: Baizx98 <baizx98@gmail.com>

--- vllm/core/layer_block_space_maneger.py
+++ vllm/core/layer_block_space_maneger.py
@@ -271,6 +271,12 @@ class LayerBlockSpaceManager(BlockSpaceManager):
 
     def get_block_table(self, seq: Sequence):
         return None
+    
+    def get_layer_block_table(self, seq: Sequence) -> List[BlockTable]:
+        layer_block_table = []
+        for layer in range(self.num_attn_layers):
+            layer_block_table.append(self.block_tables(layer)[seq.seq_id])
+        return layer_block_table
 
     def get_block_table_layer(self, seq: Sequence, layer: int) -> List[int]:
         block_ids = self.block_tables(layer)[seq.seq_id].physical_block_ids
